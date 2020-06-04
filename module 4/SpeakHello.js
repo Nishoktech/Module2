09c070c0a6ce862c060e04cb104b09142e67af18
Nishoktechs <nishok321csd@gmail.com>
2020-06-04 20:54:43 +0530

    module 4

---
+++ module 4/SpeakHello.js
@@ -0,0 +1,12 @@
+
+
+(function(window){
+	var helloSpeaker={};
+    var speakWordd = "Hello";
+    helloSpeaker.speak=function (name) {
+       console.log(speakWordd + " " + name);
+       }
+       window.helloSpeaker=helloSpeaker;
+    }
+)(window)
+
